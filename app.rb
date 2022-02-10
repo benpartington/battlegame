0852367fcedccfd399962e9a9718f099155f6fa9
Chris Middleton <ctjmiddleton@gmail.com>
2022-02-10 15:08:14 +0000

    Sessions and redirects

--- app.rb
+++ app.rb
@@ -6,15 +6,24 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1name = params[:player1]
-    @player2name = params[:player2]
-    erb(:play)
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect('/play')
   end 
+
+  get '/play' do
+    @player1name = session[:player1]
+    @player2name = session[:player2]
+    erb(:play)
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
